0f6fa346594a04d33f9fbd68eee8678f9e98972a
Juan <Erikjuan32@gmail.com>
2025-09-10 17:49:20 -0300

    UPDATE

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ function adicionarLista() {
     if (task.length <= 2) {
         alert(`Conteúdo insuficiente!`)
     } else {
-        let item = document.createElement("li")
-        item.innerText = `${task}`
-        //item é o elemento pai
-        item.appendChild(criarRemoverSpan())
-        list.appendChild(item)
-        tarefa.value = ""
-        tarefa.focus()
+      let item = document.createElement("li");
+      item.innerText = `${task}`;
+      //item é o elemento pai
+      item.appendChild(criarRemoverSpan());
+      list.appendChild(item);
+      // TRANSFORMA A LISTA EM UM ARRAY E MAPEIA TODA A LISTA
+      let itens = Array.from(list.getElementsByTagName("li")).map(
+        (li) => li.firstChild.textContent
+      );
+      // SE OS ITENS FOREM ENCONTRADOS DENTRO DA LISTA, DARÁ UM ALERT
+      if (itens.includes(task)) {
+        alert("Essa tarefa já existe na lista!");
+        item.remove();
+      }
+      tarefa.value = "";
+      tarefa.focus();
     }
 }
 list.addEventListener("click", function (event) {
